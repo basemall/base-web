68a720de5d9cc29aecc044131b86f1247b4ede3d
apollo2099 <liaoxingjian.2008@163.com>
2017-03-31 22:37:30 +0800

    公共资源jar引用测试

---
+++ src/main/java/com/ibase/mall/web/test/EmailUtilsTest.java
@@ -0,0 +1,14 @@
+package com.ibase.mall.web.test;
+
+import com.ibase.mall.utils.EmailUtils;
+
+public class EmailUtilsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+		EmailUtils.send();
+		EmailUtils.config();
+	}
+
+}

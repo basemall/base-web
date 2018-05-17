4ac9e8ae2be3baaff85baa8133310502469f4aec
liaoxiongjian <liaoxiongjian.2008@163.com>
2018-05-17 14:56:13 +0800

    新增web  访问demo示例（待完成）

---
+++ src/main/java/com/ibase/mall/web/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.ibase.mall.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @ClassName IndexController
+ * @Description TODO
+ * @Autor liaoxiongjian
+ * @Date 2018/5/14 9:06
+ * @Version 1.0
+ */
+@RestController
+public class IndexController {
+
+    //
+    @RequestMapping("/index")
+    public ModelAndView index(){
+        return null;
+    }
+}
